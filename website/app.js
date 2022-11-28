114f3e736767b7dddffc1a4ed8ae850003a200d3
Myohan <kevinshuhhh@gmail.com>
2022-11-28 20:48:32 +0800

    Update html

--- website/app.js
+++ website/app.js
@@ -10,23 +10,23 @@ const app = express();
 app.listen(8000, console.log(`App listening at http://localhost:8000`));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/manufacturer.html'));
+  res.sendFile(path.join(__dirname + '/employee.html'));
 })
 
-app.get('/image_manufacturer1', function (req, res) {
-  res.sendFile(path.join(__dirname + '/find_candidate.webp'));
+app.get('/image_employer', function (req, res) {
+  res.sendFile(path.join(__dirname + '/src/employer_find_candidate.webp'));
 })
 
-app.get('/image_supplier1', function (req, res) {
-  res.sendFile(path.join(__dirname + '/CV_Writer.webp'));
+app.get('/image_employee', function (req, res) {
+  res.sendFile(path.join(__dirname + '/src/employee_CV_Writer.webp'));
 })
 
-app.get('/supplier1', function (req, res) {
-  res.sendFile(path.join(__dirname + '/supplier1.html'));
+app.get('/employee', function (req, res) {
+  res.sendFile(path.join(__dirname + '/employee.html'));
 })
 
-app.get('/manufacturer', function (req, res) {
-  res.sendFile(path.join(__dirname + '/manufacturer.html'));
+app.get('/employer', function (req, res) {
+  res.sendFile(path.join(__dirname + '/employer.html'));
 })
 
 app.get('/query_hyperledger', function (req, res) {
